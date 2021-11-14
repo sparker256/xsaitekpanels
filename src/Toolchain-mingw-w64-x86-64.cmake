d4f1ac04526761e7ec9032181f80cfd6446dc81a
sparker256 <bgood@velocity.net>
2021-11-14 14:04:27 -0500

    Add new X-Plane SDK and hidapi

---
+++ src/Toolchain-mingw-w64-x86-64.cmake
@@ -0,0 +1,27 @@
+# Sample toolchain file for building for Windows from an Ubuntu Linux system.
+#
+# Typical usage:
+#    *) install cross compiler: `sudo apt-get install mingw-w64 g++-mingw-w64`
+#    *) cd build
+#    *) cmake -DCMAKE_TOOLCHAIN_FILE=~/Toolchain-Ubuntu-mingw32.cmake ..
+
+set(CMAKE_SYSTEM_NAME Windows)
+set(TOOLCHAIN_PREFIX x86_64-w64-mingw32)
+
+# cross compilers to use for C and C++
+set(CMAKE_C_COMPILER ${TOOLCHAIN_PREFIX}-gcc)
+set(CMAKE_CXX_COMPILER ${TOOLCHAIN_PREFIX}-g++)
+set(CMAKE_RC_COMPILER ${TOOLCHAIN_PREFIX}-windres)
+
+# target environment on the build host system
+#   set 1st to dir with the cross compiler's C/C++ headers/libs
+set(CMAKE_FIND_ROOT_PATH /usr/${TOOLCHAIN_PREFIX})
+
+# Modify default behavior of FIND_XXX() commands to search for headers/libs in
+# the target environment and search for programs in the build host environment.
+# Note that using "ONLY" causes CMake to fail finding libraries bundled in the
+# git repository, since it forcefully prepends CMAKE_FIND_ROOT_PATH to any
+# CMAKE_LIBRARY_PATH.
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY BOTH)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE BOTH)
